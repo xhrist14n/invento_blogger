bac8f4317bdb8c592ab10ed86512b41d4f5323a3
khrist14n <khrist14n@gmail.com>
2016-09-28 01:46:13 -0500

    tenplate

---
+++ scripts/clean_post.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(
+        function(){
+            $("#b-navbar").css("display","none");
+            $("#footer1").css("display","none");
+            $("#PopularPosts2 h2").css("display","none");
+        }
+        
+);
