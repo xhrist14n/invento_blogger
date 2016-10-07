a217f117f272b3c98944b43630005fdd55373d87
khrist14n <khrist14n@gmail.com>
2016-10-07 11:50:20 -0500

    template

---
+++ scripts/gallery_modals.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function(){
+    console.log("inicio gallery");
+    
+    var images = $("td").find(".gallery");
+    
+    $.each(
+        images,
+        function(index,item){
+            $(this).click(
+                function(event){
+                    console.log("click image");
+                    
+                    try{
+                        event.preventDefault();
+                    }catch(ex){}
+                }
+            );
+        }
+    );
+    
+    console.log("fin gallery");
+    
+});
+
