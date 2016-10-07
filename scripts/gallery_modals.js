5fa71d1749428db456d87ab0f15cab43a9dd75ec
khrist14n <khrist14n@gmail.com>
2016-10-07 12:07:44 -0500

    template

--- scripts/gallery_modals.js
+++ scripts/gallery_modals.js
@@ -6,6 +6,8 @@
 $(function(){
     console.log("inicio gallery");
     
+    $("#normalizer").css("display","none");
+    
     var images = $("#our_blog").find(".gallery");
     
     $.each(
@@ -14,6 +16,12 @@ $(function(){
             $(this).click(
                 function(event){
                     console.log("click image");
+                    
+                    var image = $(this).find(".modalize");
+                    
+                    var src = $(image).attr("src");
+                    
+                    $("#normalizer").css("display","");
                     
                     try{
                         event.preventDefault();
