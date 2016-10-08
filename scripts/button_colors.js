0b44a9a7b46b24da5b7ed49eb3a45a828127f642
khrist14n <khrist14n@gmail.com>
2016-10-07 19:08:06 -0500

    template

---
+++ scripts/button_colors.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function(){
+    var links = $(".btn");
+    $.each(links,function(){
+        $(this).mouseout(
+                function(){
+                    $(this).css("color","#ffffff");
+                }
+        );
+        $(this).mouseover(
+                function(){
+                    $(this).css("color","#ffffff");
+                }
+        );
+    });
+    
+});
+
