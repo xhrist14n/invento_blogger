cc61b8a110d2fb6e7a037986a824737f5f2529b0
khrist14n <khrist14n@gmail.com>
2016-11-01 18:45:51 -0500

    template

---
+++ scripts/menu.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(
+        function () {
+            $('#btn-toggle').on('click', function () {
+                $('.navbar-toggle').click() //bootstrap 3.x by Richard
+            });
+        }
+);
